1e0be157347387f7bbf8649843eee9644d0ce799
Mason Bryant <mlbryant6@gmail.com>
2021-10-31 15:32:53 -0500

    Changed favAlerts function. Made some text changes to home.html

--- profile.js
+++ profile.js
@@ -1,37 +1,21 @@
-// const favColor = event => {
-//     event.preventDefault();
-//     alert('BLUE!')
-// }
-
-// const favPlace = event => {
-//     event.preventDefault();
-//     alert('THE COLOSSEUM!')
-// }
-
-// const favRitual = event => {
-//     event.preventDefault();
-//     alert('WASHING MY HANDS WITH DIRT BEFORE BATTLE!')
-// }
-
-// document.querySelector('#color').addEventListener('click', favColor);
-
-// document.querySelector('#place').addEventListener('click', favPlace);
-
-// document.querySelector('#ritual').addEventListener('click', favRitual);
-
-const favBtns = document.querySelectorAll('button')
+const favBtns = document.querySelectorAll('button');
 
 const favAlert = event => {
     event.preventDefault();
-    if (event.target.id === 'color') {
-        alert('BLUE!');
-    } else if (event.target.id === 'place') {
-        alert('THE COLISEUM!');
-    } else if (event.target.id === 'ritual') {
-        alert('WASHING MY HANDS WITH DIRT BEFORE BATTLE!');
-    }   
-}
+    const alerts = {
+        color: 'BLUE!',
+        place: 'THE COLOSSEUM!',
+        ritual: "WASHING MY HANDS WITH DIRT BEFORE BATTLE!"
+    };
+    for (key in alerts) {
+        if (key === event.target.id) {
+            alert(alerts[key]);
+        }
+    }
+};
 
 for (i = 0; i < favBtns.length; i++) {
 favBtns[i].addEventListener('click', favAlert);
-}
+};
+
+console.log(favBtns);
